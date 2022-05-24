a222f4b032269c595c649cc9118886d8795dc5fd
Guido Solar <guido.solar.c@gmail.com>
2022-05-24 09:32:18 -0400

    Creacion de path global

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
+// Creacion de path global
+const express = require('express')
+
 const productsRouter = require('./productsRouter')
 const categoriesRouter = require('./categoriesRouter')
 const usersRouter = require('./usersRouter')
 
 function routerApp(app){
-  app.use('/products', productsRouter);
-  app.use('/categories', categoriesRouter);
-  app.use('/users', usersRouter);
+  const router = express.Router()
+  app.use('/api/v1', router)
+
+  router.use('/products', productsRouter)
+  router.use('/categories', categoriesRouter)
+  router.use('/users', usersRouter)
 }
 
 module.exports = routerApp

3e4fba8be3982555a12b75c2e80dff0d011e5001
Guido Solar <guido.solar.c@gmail.com>
2022-06-01 10:49:55 -0400

    Creation of basic CRUD

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -41,6 +41,7 @@ router.get('/fakeData', (req, res) => {
 
 })
 
+// Insert
 router.post('/', (request, response) => {
   // Parámetros a recibir
   const body = request.body
@@ -50,7 +51,39 @@ router.post('/', (request, response) => {
   })
 })
 
+// Update (parcial, no "obliga" a enviar todos los atributos)
+router.patch('/:id', (request, response) => {
+  // Parámetros a recibir
+  const { id } = request.params;
+  const body = request.body
+  response.json({
+    message: 'Partial update',
+    data: body,
+    id
+  })
+})
 
+// Update normal
+router.put('/:id', (request, response) => {
+  // Parámetros a recibir
+  const { id } = request.params;
+  const body = request.body
+  response.json({
+    message: 'Update normal',
+    data: body,
+    id
+  })
+})
 
+// Delete
+router.delete('/:id', (request, response) => {
+  // Parámetros a recibir
+  const { id } = request.params;
+  response.json({
+    message: 'Item eliminado',
+    id
+  })
+})
 
+// Exporto el módulo para su uso
 module.exports = router
